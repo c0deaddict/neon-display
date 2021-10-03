d609d22aba85e56fc8b93e73f11cf2fd9250b506
Jos van Bakel <jos@codeaddict.org>
2021-10-03 19:56:20 +0200

    live-server and followed tic-tac-toe tutorial

--- frontend/readme.md
+++ frontend/readme.md
@@ -1,2 +1,1 @@
-https://www.npmjs.com/package/@chialab/esbuild-plugin-html
 https://reactjs.org/tutorial/tutorial.html
--- frontend/scripts/build.mjs
+++ frontend/scripts/build.mjs
@@ -1,6 +1,6 @@
 import esbuild from 'esbuild';
 import htmlPlugin from '@chialab/esbuild-plugin-html';
-import http from 'http';
+import liveServer from 'live-server';
 
 let serve = false;
 
@@ -26,7 +26,6 @@ for (var arg of process.argv.slice(2)) {
     options.watch = {
       onRebuild(error, result) {
         if (error) console.error('watch build failed:', error);
-        else console.log('watch build succeeded:', result);
       },
     };
     break;
@@ -41,30 +40,13 @@ for (var arg of process.argv.slice(2)) {
   }
 }
 
-if (!serve) {
-  await esbuild.build(options);
-} else {
-  // https://esbuild.github.io/api/#customizing-server-behavior
-  const {host, port} = await esbuild.serve({
-    servedir: options.outdir
-  }, options);
+await esbuild.build(options);
 
-  http.createServer((req, res) => {
-    const options = {
-      hostname: host,
-      port: port,
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    };
-
-    const proxyReq = http.request(options, proxyRes => {
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
-
-    req.pipe(proxyReq, { end: true });
-  }).listen(3000);
-
-  console.log('esbuild serve running on http://localhost:3000');
+if (serve) {
+  liveServer.start({
+    port: 3000,
+    host: "127.0.0.1",
+    root: options.outdir,
+    open: false,
+  });
 }
