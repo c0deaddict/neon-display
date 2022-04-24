3c849ca0d5175b8ca6538b6319fcfc01e729b9e2
Jos van Bakel <jos@codeaddict.org>
2022-04-24 17:02:33 +0200

    NixOS module

--- nix/modules/neon-display/default.nix
+++ nix/modules/neon-display/default.nix
@@ -5,10 +5,11 @@ with lib;
 let
 
   cfg = config.services.neon-display;
-  configFile = {}; # TODO make json config
 
-in
-{
+  format = pkgs.formats.json { };
+  configFile = format.generate "config.json" cfg.settings;
+
+in {
   options.services.neon-display = {
     enable = mkEnableOption "neon-display";
 
@@ -17,29 +18,73 @@ in
       example = "display";
     };
 
+    group = mkOption {
+      type = types.string;
+      example = "users";
+    };
+
     package = mkOption {
       type = types.package;
       default = pkgs.neon-display;
     };
+
+    settings = mkOption {
+      default = { };
+      type = format.type;
+    };
   };
 
   config = mkIf cfg.enable {
+    services.neon-display.settings = {
+      hal_socket_path = "/var/run/neon-display/hal.sock";
+    };
+
     services.cage = {
       enable = true;
       inherit (cfg) user;
-      program = "${pkgs.neon-display}/bin/display -config ${configFile}";
+      program = "${cfg.package}/bin/display -config ${configFile}";
     };
+
+    systemd.services."cage-tty1".after = [ "neon-display-hal.service" ];
 
     systemd.services.neon-display-hal = {
       description = "neon-display hardware abstraction layer";
 
-      # TODO: make cage depend on this service.
       serviceConfig = {
-        Type = "oneshot";
+        Type = "simple";
+        ExecStart =
+          "${cfg.package}/bin/hal -hal-socket-path ${cfg.settings.hal_socket_path}";
+
+        User = "root";
+        Group = cfg.group;
+
+        RuntimeDirectoryMode = "0750";
+        RuntimeDirectory = "neon-display";
+
+        # Hardening
+        CapabilityBoundingSet = "";
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
         PrivateTmp = true;
-        # TODO: use /run dir created by systemd.
-        # TODO: tighten security a bit more?
-        ExecStart = "${pkgs.neon-display}/bin/hal -hal-socket-path /run/hal.sock";
+        PrivateUsers = true;
+        ProcSubset = "pid";
+        ProtectClock = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        ReadOnlyPaths = [ ];
+        ReadWritePaths = [ ];
+        RestrictAddressFamilies = [ "AF_UNIX" ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" ];
+        UMask = "0077";
       };
     };
   };
